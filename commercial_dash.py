9a7e36cb795fe327ed8fa3206a5674123f7f17b5
Terminal Trader <trader@terminal.com>
2026-06-07 11:16:16 -0300

    Usa proxy OANDA para juros alemao

--- commercial_dash.py
+++ commercial_dash.py
@@ -2925,8 +2925,7 @@ def pagina_correlacao():
                 ("OTCB:US02Y", "#FACC15", "2Y USA"),
                 ("OTCB:US30Y", "#00BFFF", "30Y USA"),
                 ("BMFBOVESPA:DI1F2029", "#22C55E", "DI1F2029 BR"),
-                ("TVC:JP10Y", "#A855F7", "10Y Japao"),
-                ("TVC:DE10Y", "#38BDF8", "10Y Alemanha"),
+                ("OANDA:DE10YBEUR", "#38BDF8", "10Y Alemanha"),
             ],
         )
 
